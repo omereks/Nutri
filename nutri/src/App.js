afac8bd7ed747ebe77bc443d0b7e5a464e856289
omereks <omereks@gmail.com>
2021-11-17 15:59:04 +0200

    user id identifier

--- nutri/src/App.js
+++ nutri/src/App.js
@@ -3,25 +3,31 @@ import React, { useEffect, useState } from 'react';
 import Users from './components/Users'
 import Search from './components/Search'
 import Graph from './components/Graph'
+
 function App() {
+
+  const [UserId, setUserId] = React.useState(0);
+
+
 
   return (
     <div className="App">
       <header className="App-header">
         
         <div className="user">
-          <Users/>
+          <Users chengeUserId={UserId => setUserId(UserId)}/>
         </div>
 
         <div className="bottom">
           
+          <div className="search">
+            <Search/>
+          </div>
+
           <div className="graph">
             <Graph/>
           </div>
           
-          <div className="search">
-            <Search/>
-          </div>
           
         </div>
 
--- nutri/src/components/users.js
+++ nutri/src/components/users.js
@@ -1,19 +1,86 @@
 import React, { Component } from 'react';
+import Axios from "axios";
+import './Users.css';
+import { Button,InputGroup,Input,Modal,ModalHeader,ModalBody,ModalFooter } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Toggle from "react-toggle";
+import "react-toggle/style.css";
 
 
+
+// https://reactstrap.github.io/?path=/docs/home-installation--page
 class Users extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            
+            newUser: false,
+            userID: props.userID,
+            gender: 0,
+            validID: true
         }
     }
 
+    
+
     render() {
         return (
-            <div >
-                users
+            
+            <div className="main">
+                <div className="id">
+                    <InputGroup >
+                        <Input  onChange={
+                            (val) => {
+                                this.setState({userID:`${val.target.value}`})
+                                this.setState({validID:!isNaN(`${val.target.value}`)})
+                            }}/>
+                        <Button data-bs-toggle="modal" data-bs-target="#exampleModal" {...!this.state.validID?{disabled:'true'}:""} onClick={(e)=>{
+                            if(this.state.validID){
+                                console.log(this.state.newUser)
+                                Axios.get("http://localhost:3001/api/isuser", {params: {id:this.state.userID}})
+                                .then((val)=> {
+                                    if(val.data){
+                                        this.setState({newUser: false})
+                                        this.props.chengeUserId(this.state.userID)
+                                    }else{
+                                        this.setState({newUser: true})
+                                    }
+                                })
+                            }
+                        }}>
+                        Let's Go!
+                        </Button>
+                    </InputGroup>
+                </div> 
+
+
+                <div className={this.state.newUser?"gender":"hidden"} >
+                            New User :) please select a gender:
+                            <Button data-bs-toggle="modal" data-bs-target="#exampleModal"  onClick={(e)=>{
+                            console.log(this.state.newUser)
+                            Axios.post("http://localhost:3001/api/XXXXX", {params: {id:this.state.userID}})
+                            .then((val)=> {
+                            
+                            })
+                            this.setState({newUser: false})
+                            }}>
+                                Male
+                            </Button>
+                            <span>  </span>
+                            <Button data-bs-toggle="modal" data-bs-target="#exampleModal"  onClick={(e)=>{
+                            Axios.post("http://localhost:3001/api/XXXXX", {params: {id:this.state.userID}})
+                            .then((val)=> {
+                            
+                            })
+                            this.setState({newUser: false})
+                            }}>
+                                Female
+                            </Button>
+                        </div>
+
+
             </div>
+
+            
           );
     }
 }
